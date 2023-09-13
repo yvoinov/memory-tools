4febbe55fe41a8bd9d6527088eda8a4f865345d4
Yuri Voinov <yvoinov@gmail.com>
2023-09-13 22:10:00 +0600

    Update mem_frag.sh

--- mem_frag.sh
+++ mem_frag.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
 #####################################################################################
-## Script to determine memory fragmentation (Linux only).
-## 
-## Version 1.0
+## Script to determine memory fragmentation (Linux/SunOS).
+##
+## Version 1.1
 ## Modified by Y.Voinov (C) 2023
 ## Initial written by N.Parfenovich (C) 2023
 #####################################################################################
 
-check_os()
-{
-  if [ "`uname`" != "Linux" ]; then
-    echo "ERROR: This script is for Linux only."
-    exit 2
-  fi
-}
-
-check_os
-
-awk '/MemTotal/{total=$2} /MemFree/{free=$2} /Buffers/{buf=$2} /^Cached/{cache=$2} END{printf "Fragmentation level: %.2f%%\n", (1-(free+buf+cache)/total)*100}' /proc/meminfo
+if [ "`uname`" = "Linux" ]; then
+  awk '/MemTotal/{total=$2} /MemFree/{free=$2} /Buffers/{buf=$2} /^Cached/{cache=$2} END{printf "Fragmentation level: %.2f%%\n", (1-(free+buf+cache)/total)*100}' /proc/meminfo
+elif [ "`uname`" = "SunOS" ]; then
+  echo "::memstat" | mdb -k | awk '/Total/{total=$3} /Free (freelist)/{free=$3} /Free (cachelist)/{cache=$3} /Kernel/{kernel=$3} /Anon/{anon=$3} /ZFS File Data/{file=$3} /Exec and libs/{exec=$3} /Page cache/{pcache=$3} END{printf "Fragmentation level: %.2f%%\n", (1-(free+cache+kernel+anon+file+exec+pcache)/total)*100}'
+fi
