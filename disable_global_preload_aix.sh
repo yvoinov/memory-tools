138d6d0d4fb494b57b41e60974a5bb35ad5a5672
Yuri Voinov <yvoinov@gmail.com>
2025-10-25 20:36:41 +0500

    Update disable_global_preload_aix.sh

--- disable_global_preload_aix.sh
+++ disable_global_preload_aix.sh
@@ -48,15 +48,21 @@ check_root()
 }
 
 # Main
-while [ $# -gt 0 ]; do
-  case "$1" in
-    -h|-H|\?)
-      usage_note
-    ;;
-    *) shift
-    ;;
+ # Parse command line
+if [ "x$*" != "x" ]; then
+  arg_list=$*
+  # Read arguments
+  for i in $arg_list
+  do
+    case $i in
+      -h|-H|\?)
+        usage_note
+      ;;
+      *) shift
+      ;;
     esac
-done
+  done
+fi
 
 check_os
 check_root
