268c0d4a58880421ed352201691de7e02e122d3b
Yuri Voinov <yvoinov@gmail.com>
2026-06-24 02:31:38 +0500

    Version 1.1

--- support/tools_disable_coredumps_solaris.sh
+++ support/tools_disable_coredumps_solaris.sh
@@ -1,24 +1,34 @@
 #!/sbin/sh
 
+#
 # tools_disable_coredumps_solaris.sh
 #
-# Version 1.0
+# Version 1.1
 # Written by Y.Voinov (C) 2026
+#
 
 # /var/core
 COREDIR="/var/core"
 
 # System utilities
-COREADM="`which coreadm`"
-CUT="`which cut`"
-ECHO="`which echo`"
-ID="`which id`"
-RMDIR="`which rmdir`"
-SVCADM="`which svcadm`"
-UNAME="`which uname`"
+COREADM="/bin/coreadm"
+CUT="/usr/bin/cut"
+ECHO="/usr/bin/echo"
+ID="/usr/bin/id"
+RMDIR="/usr/bin/rmdir"
+SVCADM="/usr/sbin/svcadm"
+UNAME="/usr/bin/uname"
 
 OS_VER="`$UNAME -r | $CUT -f2 -d'.'`"
 OS_NAME="`$UNAME -s | $CUT -f1 -d' '`"
+
+check_tool()
+{
+  if [ ! -x "$1" ]; then
+    $ECHO "ERROR: Required utility not found: $1"
+    exit 1
+  fi
+}
 
 check_os()
 {
@@ -29,6 +39,8 @@ check_os()
     $ECHO "ERROR: Unsupported $OS_NAME version $OS_VER. Exiting..."
     exit 1
   fi
+
+  $ECHO "[OK] Detected $OS_NAME $OS_VER"
 }
 
 check_root()
@@ -37,22 +49,56 @@ check_root()
     $ECHO "ERROR: You must be super-user to run this script."
     exit 1
   fi
+
+  $ECHO "[OK] Running as root"
 }
+
+check_tool "$COREADM"
+check_tool "$CUT"
+check_tool "$ECHO"
+check_tool "$ID"
+check_tool "$RMDIR"
+check_tool "$SVCADM"
+check_tool "$UNAME"
 
 check_os
 check_root
 
 # Check and remove directory
-[ -d "$COREDIR" ] && $RMDIR $COREDIR && \
-$ECHO "Directory $COREDIR removed"
+if [ -d "$COREDIR" ]; then
+  if $RMDIR "$COREDIR" 2>/dev/null; then
+    $ECHO "[OK] Directory $COREDIR removed"
+  else
+    $ECHO "[INFO] Directory $COREDIR not empty, leaving intact"
+  fi
+fi
 
-$COREADM -g $COREDIR/core.%f.%p -i $COREDIR/core.%f.%p -d global -d global-setid -d log -d process -d proc-setid
+$COREADM \
+  -g $COREDIR/core.%f.%p \
+  -i $COREDIR/core.%f.%p \
+  -d global \
+  -d global-setid \
+  -d log \
+  -d process \
+  -d proc-setid
+
+if [ $? -ne 0 ]; then
+  $ECHO "ERROR: Failed to configure coreadm."
+  exit 1
+fi
 
 if [ "$OS_VER" -lt "11" ]; then
   $COREADM -u
 fi
 
 $SVCADM -v restart coreadm:default
+
+if [ $? -ne 0 ]; then
+  $ECHO "ERROR: Failed to restart coreadm service."
+  exit 1
+fi
+
+$ECHO "[OK] Core dumps disabled"
 
 # Show settings
 $COREADM
--- support/tools_enable_coredumps_solaris.sh
+++ support/tools_enable_coredumps_solaris.sh
@@ -1,24 +1,34 @@
 #!/sbin/sh
 
+#
 # tools_enable_coredumps_solaris.sh
 #
-# Version 1.0
+# Version 1.1
 # Written by Y.Voinov (C) 2026
+#
 
 # /var/core
 COREDIR="/var/core"
 
 # System utilities
-COREADM="`which coreadm`"
-CUT="`which cut`"
-ECHO="`which echo`"
-ID="`which id`"
-MKDIR="`which mkdir`"
-SVCADM="`which svcadm`"
-UNAME="`which uname`"
+COREADM="/bin/coreadm"
+CUT="/usr/bin/cut"
+ECHO="/usr/bin/echo"
+ID="/usr/bin/id"
+MKDIR="/usr/bin/mkdir"
+SVCADM="/usr/sbin/svcadm"
+UNAME="/usr/bin/uname"
 
-OS_VER="`$UNAME -r|$CUT -f2 -d'.'`"
-OS_NAME="`$UNAME -s|$CUT -f1 -d' '`"
+OS_VER="`$UNAME -r | $CUT -f2 -d'.'`"
+OS_NAME="`$UNAME -s | $CUT -f1 -d' '`"
+
+check_tool()
+{
+  if [ ! -x "$1" ]; then
+    $ECHO "ERROR: Required utility not found: $1"
+    exit 1
+  fi
+}
 
 check_os()
 {
@@ -29,6 +39,8 @@ check_os()
     $ECHO "ERROR: Unsupported $OS_NAME version $OS_VER. Exiting..."
     exit 1
   fi
+
+  $ECHO "[OK] Detected $OS_NAME $OS_VER"
 }
 
 check_root()
@@ -37,22 +49,55 @@ check_root()
     $ECHO "ERROR: You must be super-user to run this script."
     exit 1
   fi
+
+  $ECHO "[OK] Running as root"
 }
+
+check_tool "$COREADM"
+check_tool "$CUT"
+check_tool "$ECHO"
+check_tool "$ID"
+check_tool "$MKDIR"
+check_tool "$SVCADM"
+check_tool "$UNAME"
 
 check_os
 check_root
 
 # Check and create directory
-[ ! -d "$COREDIR" ] && $MKDIR -p $COREDIR && \
-$ECHO "Directory $COREDIR created"
+if [ ! -d "$COREDIR" ]; then
+  $MKDIR -p "$COREDIR" || exit 1
+  $ECHO "[OK] Directory $COREDIR created"
+else
+  $ECHO "[OK] Directory $COREDIR already exists"
+fi
 
-$COREADM -g $COREDIR/core.%f.%p -i $COREDIR/core.%f.%p -e global -e global-setid -e log -e process -e proc-setid
+$COREADM \
+  -g $COREDIR/core.%f.%p \
+  -i $COREDIR/core.%f.%p \
+  -e global \
+  -e global-setid \
+  -e log \
+  -e process \
+  -e proc-setid
+
+if [ $? -ne 0 ]; then
+  $ECHO "ERROR: Failed to configure coreadm."
+  exit 1
+fi
 
 if [ "$OS_VER" -lt "11" ]; then
   $COREADM -u
 fi
 
 $SVCADM -v restart coreadm:default
+
+if [ $? -ne 0 ]; then
+  $ECHO "ERROR: Failed to restart coreadm service."
+  exit 1
+fi
+
+$ECHO "[OK] Core dumps enabled"
 
 # Show settings
 $COREADM
