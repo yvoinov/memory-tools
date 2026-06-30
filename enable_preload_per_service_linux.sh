d881636bca1d770d4c84118e62263883b1c572fc
Yuri Voinov <yvoinov@gmail.com>
2026-06-30 14:01:51 +0500

    Update enable_preload_per_service_linux.sh

--- enable_preload_per_service_linux.sh
+++ enable_preload_per_service_linux.sh
@@ -36,7 +36,7 @@ usage_note()
   echo "Usage: `basename $0` <service-name> [options]"
   echo "Options:"
   echo "    -h, -H, --help   show this help"
-  echo '    -e, -E, -e|-E "VAR1=value VAR2=value ...", extra environment variables'
+  echo '    -e|-E "VAR1=value VAR2=value ...", extra environment variables'
   echo "Example: `basename $0` apache2"
   exit 0
 }
