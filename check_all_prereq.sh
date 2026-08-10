82c0f5858a786ad7d6522bf2de1636679abebb5d
Yuri Voinov <yvoinov@gmail.com>
2026-08-10 15:17:52 +0500

    Update check_all_prereq.sh

--- check_all_prereq.sh
+++ check_all_prereq.sh
@@ -3,31 +3,30 @@
 #####################################################################################
 ## The script checks all prerequisites for custom allocator
 ##
-## Version 1.7
+## Version 1.8
 ## Written by Y.Voinov (C) 2023-2026
 #####################################################################################
 
 # Allocator library search prefix: from where to find
 LD_BASE="/usr/local"
+
 # Set library name to find
 LIB_NAME="*alloc.so"
-
-# Find allocator lib(s)
-# We assume that there is only one allocator in a given path and it has a corresponding name pattern.
-ALLOCATOR_PATH="`find $LD_BASE -name $LIB_NAME -exec file {} \; | grep 32 | cut -d':' -f1`"
-ALLOCATOR_PATH64="`find $LD_BASE -name $LIB_NAME -exec file {} \; | grep 64 | cut -d':' -f1`"
 
 # sysctl values to check (Linux only)
 SYSCTL_FILE_STR1="vm.overcommit_memory"
 OVERCOMMIT1="1"
 OVERCOMMIT2="2"
+
 # overcommit_ratio at least 70 (must be >=70)
 SYSCTL_FILE_STR2="vm.overcommit_ratio"
 OVERCOMMIT_RATIO="70"
+
 # vfs_cache_pressure no more 50 (must be <=50)
 SYSCTL_FILE_STR3="vm.vfs_cache_pressure"
 VFS_CACHE_PRESSURE="50"
 # swappiness at least 50 (must be >=50)
+
 SYSCTL_FILE_STR4="vm.swappiness"
 SWAPPINESS="50"
 
@@ -36,14 +35,15 @@ verbose="0"
 ALL_OK="0"
 
 # Subroutines
+
 usage_note()
 {
   echo "The script checks all prerequisites for custom allocator."
   echo "Must be run as root."
   echo "Usage: `basename $0` [options]"
   echo "Options:"
-  echo "    -v, -V	     Verbose. Show details."
-  echo "    -h, -H, --help   show this help"
+  echo "    -v, -V             Verbose. Show details."
+  echo "    -h, -H, --help     show this help"
   exit 0
 }
 
@@ -61,22 +61,22 @@ verbose_output()
 
 log_ok()
 {
-  printf "[OK] $*\n"
+  printf "[OK] %s\n" "$*"
 }
 
 log_nok()
 {
-  printf "[NOT OK] $*\n" >&2
+  printf "[NOT OK] %s\n" "$*" >&2
 }
 
 log_info()
 {
-  printf "[INFO] $*\n" >&2
+  printf "[INFO] %s\n" "$*" >&2
 }
 
 log_error()
 {
-  printf "[ERROR] $*\n" >&2
+  printf "[ERROR] %s\n" "$*" >&2
 }
 
 check_os()
@@ -104,21 +104,52 @@ check_root()
 
 check_lib()
 {
+  # Find allocator lib(s)
+  # We assume that there is at least one allocator in a given path
+  # and it has a corresponding name pattern.
+  ALLOCATOR_PATH="`find $LD_BASE -name $LIB_NAME -exec env POSIXLY_CORRECT=1 file {} \; | grep 32 | cut -d':' -f1`"
+  ALLOCATOR_PATH64="`find $LD_BASE -name $LIB_NAME -exec env POSIXLY_CORRECT=1 file {} \; | grep 64 | cut -d':' -f1`"
+
   if [ "$verbose" = "1" ]; then
     echo "Checking custom allocator installed..."
     echo .
-    if [ -f "$ALLOCATOR_PATH" ]; then
-      echo $ALLOCATOR_PATH
+    echo "ALLOCATOR_PATH:"
+    if [ -z "$ALLOCATOR_PATH" ]; then
+      echo "<not found>"
+    else
+      echo "$ALLOCATOR_PATH"
     fi
-    if [ -f "$ALLOCATOR_PATH64" ]; then
-      echo $ALLOCATOR_PATH64
+    echo "ALLOCATOR_PATH64:"
+    if [ -z "$ALLOCATOR_PATH64" ]; then
+      echo "<not found>"
+    else
+      echo "$ALLOCATOR_PATH64"
     fi
   fi
-  if [ ! -f "$ALLOCATOR_PATH" -a ! -f "$ALLOCATOR_PATH64" ]; then
+
+  if [ -z "$ALLOCATOR_PATH" -a -z "$ALLOCATOR_PATH64" ]; then
     log_nok "Allocator not found"
     ALL_OK="1"
-  else
-    log_ok "Allocator found"
+    return
+  fi
+
+  log_ok "Allocator found"
+
+  allocator_count="0"
+
+  if [ ! -z "$ALLOCATOR_PATH" ]; then
+    count="`echo "$ALLOCATOR_PATH" | wc -l | sed 's/[ ]*//g'`"
+    allocator_count="`expr $allocator_count + $count`"
+  fi
+
+  if [ ! -z "$ALLOCATOR_PATH64" ]; then
+    count="`echo "$ALLOCATOR_PATH64" | wc -l | sed 's/[ ]*//g'`"
+    allocator_count="`expr $allocator_count + $count`"
+  fi
+
+  if [ "$allocator_count" -gt 1 ]; then
+    log_info "Multiple allocator libraries found: $allocator_count"
+    log_info "Check ALLOCATOR_PATH/ALLOCATOR_PATH64 in verbose mode"
   fi
 }
 
@@ -127,12 +158,12 @@ check_swap()
   if [ "$os" = "SunOS" ]; then
     cmd1="`swap -l | awk -F'[^0-9]*' '$0=$5'`"
     swap_size="`expr $cmd1 \* 512`"
-    cmd2=`prtconf | grep Memory | awk '{ print $3 }'`
+    cmd2="`prtconf | grep Memory | awk '{ print $3 }'`"
     ram_size="`expr $cmd2 \* 1024 \* 1024`"
   elif [ "$os" = "Linux" ]; then
     swap_size=`free --kilo | grep Swap | awk '{ print $2 }'`
     ram_size="`grep MemTotal /proc/meminfo | awk '{ print $2 }'`"
-  elif [ "$os" =  "FreeBSD" ]; then
+  elif [ "$os" = "FreeBSD" ]; then
     cmd1="`swapinfo | grep swapfs | awk '{print $2}'`"
     swap_size="`expr $cmd1 \* 1024`"
     ram_size="`sysctl hw.physmem | awk '{ print $2 }'`"
@@ -165,9 +196,10 @@ check_recommended_vm_settings()
   if [ "$cmd1" = "$OVERCOMMIT2" ] || [ "$cmd1" = "$OVERCOMMIT1" ]; then
     log_ok "Overcommit disabled"
   else
-    log_nok "Overtommit still enabled"
+    log_nok "Overcommit still enabled"
     ALL_OK="1"
   fi
+
   cmd2="`sysctl $SYSCTL_FILE_STR2 | cut -d' ' -f3`"
   verbose_output "Checking $SYSCTL_FILE_STR2..." "Acceptable values" "$OVERCOMMIT_RATIO" "Specified value" "$cmd2"
   if [ "$cmd2" -ge "$OVERCOMMIT_RATIO" ]; then
@@ -176,6 +208,7 @@ check_recommended_vm_settings()
     log_nok "$SYSCTL_FILE_STR2"
     ALL_OK="1"
   fi
+
   cmd3="`sysctl $SYSCTL_FILE_STR3 | cut -d' ' -f3`"
   verbose_output "Checking $SYSCTL_FILE_STR3..." "Acceptable values" "$VFS_CACHE_PRESSURE" "Specified value" "$cmd3"
   if [ "$cmd3" -le "$VFS_CACHE_PRESSURE" ]; then
@@ -184,6 +217,7 @@ check_recommended_vm_settings()
     log_nok "$SYSCTL_FILE_STR3"
     ALL_OK="1"
   fi
+
   cmd4="`sysctl $SYSCTL_FILE_STR4 | cut -d' ' -f3`"
   verbose_output "Checking $SYSCTL_FILE_STR4..." "Acceptable values" "$SWAPPINESS" "Specified value" "$cmd4"
   if [ "$cmd4" -ge "$SWAPPINESS" ]; then
@@ -197,24 +231,53 @@ check_recommended_vm_settings()
 check_ld_conditions()
 {
   res="0"
+  ld_matches=""
+  ld_matches64=""
+
   if [ "$os" = "SunOS" ]; then
-    if [ ! -z "`crle | grep $ALLOCATOR_PATH`" -a ! -z "`crle -64 | grep $ALLOCATOR_PATH64`" ]; then
-      res=1
+    ld_matches="`crle | grep 'alloc.so'`"
+    ld_matches64="`crle -64 | grep 'alloc.so'`"
+    if [ ! -z "$ld_matches" -o ! -z "$ld_matches64" ]; then
+    res="1"
     fi
   elif [ "$os" = "Linux" ]; then
-    dir1="`dirname $ALLOCATOR_PATH 2>/dev/null`"
-    dir2="`dirname $ALLOCATOR_PATH64 2>/dev/null`"
-    if [ ! -z "`ldconfig -p | grep $dir1 2>/dev/null`" -o ! -z "`ldconfig -p | grep $dir2 2>/dev/null`" ]; then
-      res=1
+    ld_matches="`ldconfig -p | grep 'alloc.so' 2>/dev/null`"
+    if [ ! -z "$ld_matches" ]; then
+      res="1"
     fi
   elif [ "$os" = "FreeBSD" ]; then
-    dir1="`dirname $ALLOCATOR_PATH 2>/dev/null`"
-    dir2="`dirname $ALLOCATOR_PATH64 2>/dev/null`"
-    if [ ! -z "`ldconfig -r | grep $dir1 2>/dev/null`" -o ! -z "`ldconfig -r | grep $dir2 2>/dev/null`" ]; then
-      res=1
+    ld_matches="`ldconfig -r | grep 'alloc.so' 2>/dev/null`"
+    if [ ! -z "$ld_matches" ]; then
+      res="1"
     fi
   fi
-  verbose_output "Checking LD.SO conditions..." "LD.SO prerequisites" "$res"
+
+  if [ "$verbose" = "1" ]; then
+    echo .
+    echo "Checking LD.SO conditions..."
+    if [ "$os" = "SunOS" ]; then
+      echo "LD.SO 32-bit:"
+      if [ -z "$ld_matches" ]; then
+        echo "<no allocator>"
+      else
+        echo "$ld_matches"
+      fi
+      echo "LD.SO 64-bit:"
+      if [ -z "$ld_matches64" ]; then
+        echo "<no allocator>"
+      else
+        echo "$ld_matches64"
+      fi
+    else
+      echo "LD.SO:"
+      if [ -z "$ld_matches" ]; then
+        echo "<no allocator>"
+      else
+        echo "$ld_matches"
+      fi
+    fi
+  fi
+
   if [ "$res" = "1" ]; then
     log_ok "LD.SO prerequisites"
   else
@@ -224,19 +287,23 @@ check_ld_conditions()
 }
 
 # Main
- # Parse command line
+
+# Parse command line
 if [ "x$*" != "x" ]; then
   arg_list=$*
-  # Read arguments
+
   for i in $arg_list
   do
     case $i in
-     -v|-V) verbose="1"
-     ;;
-     -h|-H|--help) usage_note
-     ;;
-     *) shift
-     ;;
+      -v|-V)
+        verbose="1"
+      ;;
+      -h|-H|--help)
+        usage_note
+      ;;
+      *)
+        shift
+      ;;
     esac
   done
 fi
